8814230a272b927865ddde9d92bf0298b9f408ca
kullervo-wanona <kullervo-wanona@gmail.com>
2022-05-14 19:55:00 -0400

    “kullervo”

--- experiment.py
+++ experiment.py
@@ -56,7 +56,7 @@ n_in=train_data_loader.image_size[3]
 # flow_net = GenerativeSchurFlow(c_in, n_in, k_list=[10, 10, 10, 10, 10], squeeze_list=[0, 0, 0, 0, 0])
 # flow_net = GenerativeSchurFlow(c_in, n_in, k_list=[10, 10, 10, 10, 10, 10, 10, 10, 10, 10], squeeze_list=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 flow_net = GenerativeConditionalSchurFlow(c_in, n_in, n_blocks=20)
-# flow_net.set_actnorm_parameters(train_data_loader, setup_mode='Training', n_batches=3, test_normalization=False)
+flow_net.set_actnorm_parameters(train_data_loader, setup_mode='Training', n_batches=5, test_normalization=False)
 
 n_param = 0
 for name, e in flow_net.named_parameters():
